9b0d28ea2205d3374243ee04d5d0ff5cc2f44301
Lucas Sousa Alves <lucas.sousa277@hotmail.com>
2022-10-20 11:55:21 -0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var password = document.querySelector("#exampleInputPassword1");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#");
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {};
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender"){
+
+        if (field.checked) user[field.name] = field.value;
+
+    } else{ 
+
+        user[field.name] = field.value;
+    }
+
+});
+
