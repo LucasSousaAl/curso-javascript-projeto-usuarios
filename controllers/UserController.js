bf7749a2890e85cc9101a19056d07bcdddd6957e
Lucas Sousa Alves <lucas.sousa277@hotmail.com>
2022-10-24 14:01:43 -0300

    10/24/2022

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,19 +11,43 @@ class UserController{
     addLine(dataUser){
         
         let tr = document.createElement("tr");
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
                         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
                         <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
-                        <td>${dataUser.birth}</td>
+                        <td>${Utils.dateFormat(dataUser.register)}</td>
                         <td>
                           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                         </td>
         `;
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
     
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdm = 0;
+
+        [...this.tableEl.children].forEach(tr =>{
+
+            numberUsers++;
+
+        let user = JSON.parse(tr.dataset.user);
+
+        if (user._admin) numberAdm++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-adm").innerHTML = numberAdm;
     }
 
     onSubmit(){
@@ -32,7 +56,13 @@ class UserController{
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();
+
+            if(!values) return false;
 
             this.getPhoto().then(
                 (content)=>{
@@ -40,6 +70,10 @@ class UserController{
                     values.photo = content;
 
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
                 
                 },
                 (e)=>{
@@ -90,10 +124,17 @@ class UserController{
     getValues(){
 
         let user = {};
-
+        let isValid = true;
         //let spread =  [...this.formEl.elements];
 
         [...this.formEl.elements].forEach((field, index) =>{
+
+            if(["name", "email", "password"].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add("has-error");
+                isValid = false;
+
+            }
 
             if (field.name == "gender"){
         
@@ -109,8 +150,14 @@ class UserController{
             }
         
         });
+
+        if(!isValid){
+
+            return false;
+
+        }
     
-        return new User(user.name, user.gender, user.birth, user.email, user.password, user.photo, user.admin);
+        return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
 
     }
 
